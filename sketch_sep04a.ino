f3ab4f00b3277c51326deaa6af071b0194320bca
AdityaChanan <83080295+AdityaChanan@users.noreply.github.com>
2021-09-04 11:33:09 +0530

    Add files via upload

---
+++ sketch_sep04a.ino
@@ -0,0 +1,52 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  //A
+  dot();
+  dash();
+  
+  //D
+  dash();
+  dot();
+  dot();
+  
+  //I
+  dot();
+  dot();
+  
+  //T
+  dash();
+  
+  //Y
+  dash();
+  dot();
+  dash();
+  dash();
+  
+  //A
+  dot();
+  dash();
+}
+
+void dot()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+
+void dash()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(3000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+  
